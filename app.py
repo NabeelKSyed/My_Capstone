94728d46df3d76133453f93fc2460b86fe251713
Nabeel Kamal Syed <121056674+NabeelKSyed@users.noreply.github.com>
2025-04-09 14:59:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, send_from_directory
+from flask import Flask, Response, send_from_directory
+import boto3
 import os
-
 app = Flask(__name__)
-
+s3 = boto3.client('s3')
+# Replace with your bucket + keys
+BUCKET = 'capstone-outputs-bucket'
+INDEX_KEY = 'output/index.html'
+NEWSFEED_KEY = 'output/newsfeed-2.html'
 @app.route('/')
-def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/<path:filename>')
+def serve_index():
+    obj = s3.get_object(Bucket=BUCKET, Key=INDEX_KEY)
+    html = obj['Body'].read().decode('utf-8')
+    return Response(html, mimetype='text/html')
+@app.route('/newsfeed-2')
+def serve_newsfeed():
+    obj = s3.get_object(Bucket=BUCKET, Key=NEWSFEED_KEY)
+    html = obj['Body'].read().decode('utf-8')
+    return Response(html, mimetype='text/html')
+@app.route('/static/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('.', filename)
+    return send_from_directory('static', filename)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
